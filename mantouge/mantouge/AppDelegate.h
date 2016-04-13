277236c52aa9d0b9a23fd465884c160781bd9635
hwhe <hwhe@hwhedemac-mini.local>
2016-04-13 15:15:47 +0800

    me

---
+++ mantouge/mantouge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mantouge
+//
+//  Created by hwhe on 16/4/13.
+//  Copyright © 2016年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mantouge/mantouge/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mantouge
+//
+//  Created by hwhe on 16/4/13.
+//  Copyright © 2016年 mm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Hello");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
